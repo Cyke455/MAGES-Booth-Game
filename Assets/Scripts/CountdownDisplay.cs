45e60eeaa46da7bff071cdebc83bccff4efa4eb8
sen-taipei <taipeistreets@gmail.com>
2026-07-27 16:47:19 +0800

    countdown fix

--- Assets/Scripts/CountdownDisplay.cs
+++ Assets/Scripts/CountdownDisplay.cs
@@ -8,13 +8,9 @@ public class CountdownDisplay : MonoBehaviour
     [SerializeField] private Transform run;
     [SerializeField] private Image runImage;
 
-    Vector3 defaultSize;
-    Vector3 runSize;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        defaultSize = transform.localScale;
-        runSize = run.localScale;
     }
 
     public void DisplayNumber(int number)
@@ -26,14 +22,14 @@ public class CountdownDisplay : MonoBehaviour
         image.sprite = numbers[number];
         image.color = new Color(1f, 1f, 1f, 0f);
         transform.rotation = Quaternion.Euler(0, 0, 20);
-        transform.localScale = defaultSize * 0.5f;
+        transform.localScale = new Vector3(1, 1, 1) * 0.5f;
     }
 
     public void DisplayStart()
     {
         image.enabled = false;
         runImage.enabled = true;
-        run.localScale = runSize * 0.75f;
+        run.localScale = new Vector3(1, 1, 1) * 0.75f;
         runImage.color = Color.white;
     }
     // Update is called once per frame
@@ -43,12 +39,12 @@ public class CountdownDisplay : MonoBehaviour
         float movementAlpha = Mathf.Clamp(1f - Mathf.Exp(-Time.deltaTime / 0.3f), 0, 1);
         image.color = Color.Lerp(image.color, new Color(1, 1, 1, 1f), colorAlpha);
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, movementAlpha);
-        transform.localScale = Vector3.Lerp(transform.localScale, defaultSize, movementAlpha);
+        transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(1, 1, 1), movementAlpha);
 
         float runColorAlpha = Mathf.Clamp(1f - Mathf.Exp(-Time.deltaTime / 0.4f), 0, 1);
         float runMovementAlpha = Mathf.Clamp(1f - Mathf.Exp(-Time.deltaTime / 0.5f), 0, 1);
         runImage.color = Color.Lerp(runImage.color, new Color(1, 1, 1, 0f), runColorAlpha);
-        run.localScale = Vector3.Lerp(run.localScale, runSize, runMovementAlpha);
+        run.localScale = Vector3.Lerp(run.localScale, new Vector3(1, 1, 1), runMovementAlpha);
         runImage.enabled = (runImage.color.a > 0.01f);
 
     }
